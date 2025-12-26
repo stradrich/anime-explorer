7a73e89abaff57aa0291b8262301adc34ee93694
Aldrich <aldrichpinso@gmail.com>
2025-12-26 16:39:29 +0800

    feat: anime display count; load more btn; infinity scroll; update loading skeleton behavior;

--- src/components/AnimeMainList.tsx
+++ src/components/AnimeMainList.tsx
@@ -2,15 +2,62 @@ import LoadingSkeleton from "../components/ui/loading-skeleton"
 import AnimeCard from "./AnimeCard"
 import type { Anime } from "../types/anime"
 import { mockAnimeArr } from "../types/mockData/anime"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
+
+const ANIME_DISPLAY_COUNT = 8;
 
 export default function AnimeMainList() {
   const [loading, setLoading] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(ANIME_DISPLAY_COUNT);
+  const [infiniteScrollEnabled, setInfiniteScrollEnabled] = useState(false);
+ 
+  const loaderRef = useRef<HTMLDivElement | null>(null);
 
+  // Initial loading
   useEffect(() => {
     setLoading(true);
     setTimeout(() => setLoading(false), 100);
   }, []);
+
+  const visibleAnimes = mockAnimeArr.slice(0, visibleCount);
+
+  // Load more 
+  const loadMore = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setVisibleCount(prev => prev + ANIME_DISPLAY_COUNT)
+      setInfiniteScrollEnabled(true); // switch to infinity scroll after clicking load more btn
+      setLoading(false);
+    }, 500) // simulate delay
+  }
+
+  // Infinite Scroll
+  useEffect(() => {
+    if (!infiniteScrollEnabled) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (
+          entries[0].isIntersecting &&
+          visibleCount < mockAnimeArr.length &&
+          !loading
+        ) {
+          setLoading(true);
+          setTimeout(() => {
+            setVisibleCount((prev) => prev + ANIME_DISPLAY_COUNT);
+            setLoading(false);
+          }, 500);
+        }
+      },
+      {
+        rootMargin: "200px",
+      }
+    );
+
+    if (loaderRef.current) observer.observe(loaderRef.current);
+
+    return () => observer.disconnect();
+  }, [infiniteScrollEnabled, visibleCount, loading]);
 
   return (
     <section className="container">
@@ -33,39 +80,44 @@ export default function AnimeMainList() {
       </div>
 
       {/* Content Area */}
-      <div>
-        {loading ? (
-          <LoadingSkeleton />
-        ) : (
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fill, minmax(180px, 1fr))",
-            gap: "1rem",
-          }}
-        >
-          {mockAnimeArr.map((anime) => (
-            <AnimeCard anime={anime}/>
-          ))}
-        </div>
-        )
-        }
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(180px, 1fr))",
+          gap: "1rem",
+        }}
+      >
+        {visibleAnimes.map((anime) => (
+          <AnimeCard key={anime.id} anime={anime} />
+        ))}
+        {loading && <LoadingSkeleton count={ANIME_DISPLAY_COUNT} />}
       </div>
 
       {/* Pagination / Load More */}
-      <div style={{ marginTop: "2rem", textAlign: "center" }}>
-        <button
-          // disabled
-          style={{
-            padding: "0.75rem 1.5rem",
-            borderRadius: "0.5rem",
-            backgroundColor: "#e5e7eb",
-            cursor: "not-allowed",
-          }}
-        >
-          Load more
-        </button>
-      </div>
+      {!infiniteScrollEnabled && (
+        <div style={{ marginTop: "2rem", textAlign: "center" }}>
+          <button
+            onClick={loadMore}
+            // disabled
+            style={{
+              padding: "0.75rem 1.5rem",
+              borderRadius: "0.5rem",
+              backgroundColor: "#e5e7eb",
+              // cursor: "not-allowed",
+            }}
+          >
+            Load more
+          </button>
+        </div>
+      )}
+
+      {/* Infinite scroll sentinel */}
+      {infiniteScrollEnabled && (
+        <div
+          ref={loaderRef}
+          style={{ height: "40px", marginTop: "2rem" }}
+        />
+      )}
     </section>
   )
 }
