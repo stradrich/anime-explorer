02749e756f171df664c8e12c83a80bcc32c986a3
Aldrich <aldrichpinso@gmail.com>
2025-12-28 16:12:12 +0800

    feat: match model

--- src/types/anime.ts
+++ src/types/anime.ts
@@ -1,9 +1,23 @@
 // minimalised jikan payload, only necessary field used for anime cards
-export interface Anime {
-    id: number
-    title: string
-    imageUrl: string
-    score: number | null
-    year: number | null
-    genres: string[]
+// export interface Anime {
+//     id: number
+//     title: string
+//     imageUrl: string
+//     score: number | null
+//     year: number | null
+//     genres: string[]
+// }
+
+
+export type Anime = {
+  id: number;
+  title: string;
+  imageUrl: string;
+  synopsis: string;
+  episodes: number;
+  score: number;
+  type: string;
+  rating: string;
+  year: number | null;
+  genres: string[];
 }
