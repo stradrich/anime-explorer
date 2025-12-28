3e835daa4f54bc54c6459b43e5109a74391271af
Aldrich <aldrichpinso@gmail.com>
2025-12-28 16:08:05 +0800

    feat: provide anime context

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import AnimeMainList from './components/AnimeMainList';
 import AnimeDetailedPage from './components/AnimeDetailedPage';
 import FavoritePage from './components/FavouritePage';
 import { FavouritesProvider } from "./context/FavouritesContext";
+import { AnimeProvider } from "./context/AnimeContext"; // <- add this
 
 export default function App() {
   return (
     <FavouritesProvider>
-      <BrowserRouter>
-        <Routes> 
-          <Route element={<Layout/>}>
-            <Route path="/" element={<AnimeMainList/>}/>
-            <Route path="/anime/:id" element={<AnimeDetailedPage/>}/>
-            <Route path="/favorites" element={<FavoritePage/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AnimeProvider>
+        <BrowserRouter>
+          <Routes> 
+            <Route element={<Layout/>}>
+              <Route path="/" element={<AnimeMainList/>}/>
+              <Route path="/anime/:id" element={<AnimeDetailedPage/>}/>
+              <Route path="/favorites" element={<FavoritePage/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AnimeProvider>
     </FavouritesProvider>
   )
 };
